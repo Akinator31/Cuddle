98ff9e1ccab13e5f4488669e19b1c64b14cea482
Pavel <pavel.de-wavrechin@epitech.eu>
2025-03-10 18:03:22 +0100

    feat(df_tail): add df_tail function

---
+++ src/basics_operations/df_tail.c
@@ -0,0 +1,83 @@
+/*
+** EPITECH PROJECT, 2025
+** G-AIA-200-TLS-2-1-cuddle-pavel.de-wavrechin
+** File description:
+** df_tail
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include "dataframe.h"
+#include "utils.h"
+
+static void dump_column_content_from_tail(
+    column_t *column_src,
+    column_t *column_dest,
+    size_t expected_rows,
+    size_t nb_rows)
+{
+    size_t e = 0;
+
+    if (!column_dest || !column_dest->content_strings)
+        return;
+    if (!nb_rows)
+        return;
+    for (size_t i = (size_t)nb_rows - 1; i >= nb_rows - expected_rows; i--) {
+        column_dest->content_strings[e] =
+            strdup(column_src->content_strings[i]);
+        column_dest->content[e] = column_src->content[i];
+        e++;
+        if (!i)
+            return;
+    }
+}
+
+static column_t **dump_columns_from_tail(
+    dataframe_t *df_src,
+    dataframe_t *df_dest)
+{
+    size_t i = 0;
+    column_t **data = malloc(sizeof(column_t *) * df_src->nb_columns);
+
+    while (i < df_src->nb_columns) {
+        data[i] = malloc(sizeof(column_t));
+        if (!data[i])
+            return NULL;
+        data[i]->name = strdup(df_src->columns[i]->name);
+        data[i]->type = df_src->columns[i]->type;
+        data[i]->content_strings = malloc(sizeof(char *) * df_src->nb_rows);
+        data[i]->content = malloc(sizeof(column_content_t) * df_src->nb_rows);
+        dump_column_content_from_tail(df_src->columns[i], data[i],
+            df_dest->nb_rows, df_src->nb_rows);
+        i++;
+    }
+    return data;
+}
+
+static dataframe_t *dump_dataframe_from_tail(
+    dataframe_t *df_src,
+    size_t nb_rows)
+{
+    size_t dataframe_size = nb_rows;
+    dataframe_t *new_dataframe = NULL;
+
+    if (!df_src)
+        return NULL;
+    if (nb_rows > df_src->nb_rows)
+        dataframe_size = df_src->nb_rows;
+    new_dataframe = create_dataframe(dataframe_size, df_src->nb_columns);
+    new_dataframe->columns = dump_columns_from_tail(df_src, new_dataframe);
+    return new_dataframe;
+}
+
+dataframe_t *df_tail(
+    dataframe_t *dataframe,
+    int nb_rows)
+{
+    dataframe_t *new_dataframe = NULL;
+
+    if (!dataframe || nb_rows < 0)
+        return NULL;
+    new_dataframe = dump_dataframe_from_tail(dataframe, (size_t)nb_rows);
+    return new_dataframe;
+}
