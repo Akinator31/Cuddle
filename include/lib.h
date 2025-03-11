0a9931693e3d4bf92f2944c4d4bbec7596b33d3b
Gaël Laguna <gael.laguna@epitech.eu>
2025-03-11 18:47:21 +0100

    add (df_read_csv.c) warning on duplicate column names

--- include/lib.h
+++ include/lib.h
@@ -10,11 +10,13 @@
 
     #include <stddef.h>
     #include <stdio.h>
+    #include <stdbool.h>
 
 char **str_to_word_array(const char *str, const char *delim);
 void print_str_array(char **array);
 size_t str_array_len(char **array);
 void *free_str_array(char **array);
+bool str_in_array(char *str, char **array);
 
 // getline wrapper to remove newline characters
 ssize_t getlinex(
