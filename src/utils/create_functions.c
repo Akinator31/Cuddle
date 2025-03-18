f9c95d1318356e5d9d1e61b516f1d5465b471692
Gaël Laguna <gael.laguna@epitech.eu>
2025-03-18 13:16:47 +0100

    fix (create_functions.c) removed memory leak (coming from garbage collector)

--- src/utils/create_functions.c
+++ src/utils/create_functions.c
@@ -9,25 +9,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dataframe.h"
-#include "garbage_collector.h"
 #include "utils.h"
 
 static column_t *create_columns(size_t nb_columns, size_t nb_rows)
 {
-    column_t *column_list = my_malloc(sizeof(column_t) * nb_columns);
+    column_t *column_list = malloc(sizeof(column_t) * nb_columns);
 
     for (size_t i = 0; i < nb_columns; i++) {
         column_list[i].type = UNDEFINED;
         column_list[i].name = NULL;
         column_list[i].content = NULL;
-        column_list[i].content_strings = my_calloc(nb_rows, sizeof(char *));
+        column_list[i].content_strings = calloc(nb_rows, sizeof(char *));
     }
     return column_list;
 }
 
 dataframe_t *create_dataframe(size_t nb_rows, size_t nb_columns, char *delim)
 {
-    dataframe_t *dataframe = my_malloc(sizeof(dataframe_t));
+    dataframe_t *dataframe = malloc(sizeof(dataframe_t));
 
     dataframe->nb_rows = nb_rows;
     dataframe->nb_columns = nb_columns;
